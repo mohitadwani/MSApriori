d57caf3ee5ca32d4b5f96d1b3072a006b856a55f
Mohit Adwani <mohitadwani@gmail.com>
2018-02-16 19:28:52 -0600

    Added two functions prune

--- main.py
+++ main.py
@@ -41,10 +41,8 @@ def read_param_file():
     file = open("parameter-file.txt", "r")
     for line in file:
         if 'must' in line:
-    #        temp.append(line.replace(' ', '').replace('\n', '').split(':')[1])
             if 'or' in line:
                 for i in line.replace(' ', '').replace('\n', '').split(':')[1].split('or'):
-        #            print (i)
                     must_have_items.append(int(i))
             else:
                 must_have_items.append(int(line.replace(' ', '').replace('\n', '').split(':')[1]))
@@ -71,31 +69,32 @@ def ms_apriori(transactions_list, min_support, SDC, must_have_items, cannot_be_t
     n = len(transactions_list)
     F = []
     C = []
-    count = {}
-    tail_count = {}
+    count_dict = {}
+    tail_count_dict = {}
     min_support = dict(sorted(min_support.items(), key=lambda x:x[1]))
-    print("Sorted Itemset is ")
-    print(min_support)
+    print("Sorted Itemset is {}".format(min_support))
     # Step 2 is to calculate L using init_pass
     l = init_pass(min_support, transactions_list)
-    f1 = dict()
+    f1 = {}
     for item, act_sup in l.items():
         if act_sup > min_support[item]:
             f1[item] = act_sup
-    print(f1)
+    print("F1 = {}".format(f1))
     k = 2
     while True:
         if k == 2:
             c2 = level2_can_gen(l, SDC, min_support, transactions_list)
             C.append(c2)
-            print(c2)
+            print("C2 = {}".format(c2))
         else:
             ck = MSCandidate_can_gen(F[k-3], l, SDC, min_support, transactions_list)
+            if len(ck) == 0:
+                break
             C.append(ck)
-        # print(C[k-2])
+        print("Ck = {}".format(C[k-2]))
         for candidate in C[k-2]:
-            count[candidate] = 0
-            tail_count[candidate] = 0
+            count_dict[candidate] = 0
+            tail_count_dict[candidate] = 0
         for transaction in transactions_list:
             for candidate in C[k-2]:
                 temp_count = 0
@@ -103,27 +102,35 @@ def ms_apriori(transactions_list, min_support, SDC, must_have_items, cannot_be_t
                     if item in transaction:
                         temp_count +=1
                 if temp_count == len(candidate):
-                    count[candidate] += 1
+                    count_dict[candidate] += 1
                 temp_count = 0
                 for item in candidate[1:]:
                     if item in transaction:
                         temp_count +=1
                 if temp_count == len(candidate[1:]):
-                    tail_count[candidate] += 1
+                    tail_count_dict[candidate] += 1
                 # if list(candidate) in transaction:
                 #     print("suckjsdbnfjksdbjkfbskdnfksdnkf")
                     # count[candidate] += 1
         fk = []
         for candidate in C[k-2]:
-            if count[candidate]/n >= min_support[candidate[0]]:
+            if count_dict[candidate]/n >= min_support[candidate[0]]:
                 fk.append(candidate)
-        print(count)
-        print(fk)
-        F.append(fk)
-        if k == 3: # replace this with F k -1 = empty
+        print("Count Dict = {}".format(count_dict))
+        print("Fk = {}".format(fk))
+        if len(fk) == 0:
             break
+        F.append(fk)
         k += 1
 
+    print("**************************MSApriori OVER**********************")
+    print("C = {}".format(C))
+    print("F = {}".format(F))
+    print("Count = {}".format(count_dict))
+    if must_have_items:
+        F = prune_must_have(F, must_have_items)
+    if cannot_be_together:
+        F = prune_cannot_be_together(F, cannot_be_together)
 def init_pass(min_support, transactions_list):
     # to add first item in L, that item will be the first item which satisfies it's min_support
     l = dict()
@@ -164,9 +171,9 @@ def level2_can_gen(l, SDC, min_support, transactions_list):
 def MSCandidate_can_gen(f, l, SDC, min_support, transactions_list):
     print("inside mscan gen")
     c = []
-    f1 = list(f)
-    f1.remove((140,120))
-    print(f1)
+    # f_something = list(f)
+    # f_something.remove((100,120))
+    # print("f_something removed with one subset to test = {}".format(f_something))
     for i, f1 in enumerate(f):
         temp_f = []
         # print(i, f1)
@@ -177,16 +184,21 @@ def MSCandidate_can_gen(f, l, SDC, min_support, transactions_list):
                 c.append(temp_f)
                 for subset in list(combinations(temp_f, len(temp_f)-1)):
                     if temp_f[0] in subset or (min_support[temp_f[1]] == min_support[temp_f[0]]):
-                        if subset in f: # this doesnt work
-                            print("sdkjvfbjdskfnfgkdsfngndkjfng")
-                            print(subset)
-
-
-
-
+                        print(subset)
+                        if subset not in f: # this doesnt work
+                            print("Not In F : {}".format(subset))
+                            c.remove(temp_f)
+                            break
     print("mscan gen ends")
     return c
 
+def prune_must_have(F, must_have_items):
+    print(F)
+    return F
 
+def prune_cannot_be_together(F, cannot_be_together):
+    print("inside cannot_be_together")
+    print(F)
+    return F
 
 main()
